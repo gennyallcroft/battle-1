cf2ded062545c85babd1da7e6b4629b653d3af3d
AnastasiiaBlaha <anastasiia.blaha@gmail.com>
2019-07-11 16:18:08 +0100

    Skinny Controllers: refactored the controller

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,26 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
-  let(:player1) { double :player }
-  let(:player2) { double :player, hit_points: 60, reduce_hit_points: nil}
+  subject(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double :player }
+  let(:player_2) { double :player, hit_points: 60, reduce_hit_points: nil}
+
+  describe '#player_1' do
+      it 'retrieves the first player' do
+        expect(game.player_1).to eq player_1
+      end
+    end
+  
+    describe '#player_2' do
+      it 'retrieves the second player' do
+        expect(game.player_2).to eq player_2
+      end
+    end 
 
   describe '#attack' do
     it 'reduces hit points of opponent' do
-      expect(player2).to receive(:reduce_hit_points)
-      game.attack(player2)
+      expect(player_2).to receive(:reduce_hit_points)
+      game.attack(player_2)
     end
   end
   
